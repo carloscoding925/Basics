10d983fb88177a937a476a25c8e6bd1a00a87002
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-23 20:22:41 -0800

    Readme and Java folder

---
+++ Java/JavaCheetSheet.java
@@ -0,0 +1,7 @@
+package Java;
+
+public class JavaCheetSheet {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Java Cheet Sheet");
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Dedicated space for practicing code
