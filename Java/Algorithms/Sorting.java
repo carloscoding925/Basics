54f81da4e3ba21540598393a933436975fbd93cc
Carlos Hernandez <carlosandresh925@gmail.com>
2025-09-18 11:01:19 -0700

    Add sorting file

---
+++ Java/Algorithms/Sorting.java
@@ -0,0 +1,22 @@
+package Java.Algorithms;
+
+public class Sorting {
+    public static void main(String args[]) {
+        System.out.println("--- This java class will go over common sorting algorithms ---");
+
+        int[] randArray = {64, 34, 25, 12, 22, 11, 90, 5};
+
+        bubbleSort(randArray);
+
+        return;
+    }
+
+    private static void bubbleSort(int[] array) {
+        int length = array.length;
+        for (int i = 0; i < length - 1; i++) {
+            
+        }
+
+        return;
+    }
+}
