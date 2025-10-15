ab52c9610b784d2f7c48085123d3085feea90b23
Carlos Hernandez <carlosandresh925@gmail.com>
2025-10-14 22:13:59 -0700

    add hashmap file

---
+++ Java/DataStructures/DS_HashMaps.java
@@ -0,0 +1,7 @@
+package Java.DataStructures;
+
+public class DS_HashMaps {
+    public static void main(String[] args) {
+        System.out.println("This java class will go over common hashmap operations");
+    }
+}
